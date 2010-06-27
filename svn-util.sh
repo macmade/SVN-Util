4210858b8d4c15168fd0b8eaf29f5303acbeea81
macmade@eosgarden.com <macmade@eosgarden.com@cc092f70-2df4-11df-a3dc-1b164710d598>
2010-06-27 10:36:17 +0000

    Cleaning...
    git-svn-id: https://svn-util.googlecode.com/svn/trunk@3 cc092f70-2df4-11df-a3dc-1b164710d598

--- svn-util.sh
+++ svn-util.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ################################################################################
-# Copyright (c) 2009, Jean-David Gadina <macmade@eosgarden.com>                #
+# Copyright (c) 2010, Jean-David Gadina <macmade@eosgarden.com>                #
 # All rights reserved.                                                         #
 #                                                                              #
 # Redistribution and use in source and binary forms, with or without           #
